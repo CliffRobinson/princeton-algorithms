30d44b6fbd39c9df462f3fbc91b01d46b391e2d1
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-09-03 17:16:41 +1200

    Moved testing files to own class

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,8 +1,4 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.io.File;
-//import java.io.FileNotFoundException;
-import java.io.FilenameFilter;
-import java.util.Scanner;
 
 public class Percolation {
 
@@ -138,70 +134,9 @@ public class Percolation {
 	
 	
 	public static void main(String[] args) {
-		File aDirectory = new File("./src/testData");
 
-		FilenameFilter fileNameFilter = new FilenameFilter() {
-			@Override
-			public boolean accept(File dir, String name) {
-				if(name.lastIndexOf('.')>0) {
-					// get last index for '.' char
-					int lastIndex = name.lastIndexOf('.');
-					// get extension
-					String str = name.substring(lastIndex);
-					// match path name extension
-					if(str.equals(".txt")) {
-						return true;
-					}
-				}
-				return false;
-			}
-		};	    
-		// get a listing of all files in the directory
-		String[] filesInDir = aDirectory.list(fileNameFilter);
-
-		// have everything i need, just print it now
-		for ( int i=0; i<filesInDir.length; i++ ) {
-			//System.out.printf( "file %d: %s \n", i,  filesInDir[i] );
-			File sample = new File("./src/testData/"+filesInDir[i]);
-			test(sample);
-		}
-		System.out.println("");
-
-
-
+		
 	}
-	private static void test(File sample) {
-		try {
-			Scanner scn = new Scanner(sample);
 
-			System.out.print("Name: " + sample.getName());
-
-
-			int n = scn.nextInt();
-
-			Percolation p = new Percolation(n);
-
-			while(scn.hasNextInt()) {
-				int i = scn.nextInt();
-				int j = scn.nextInt();
-				p.open(i, j);
-			}
-			scn.close();
-
-
-			if (p.percolates()) {
-				System.out.print(" - Percolates.\n");
-			} else {
-				System.out.print(" - Does not percolate.\n");
-			}
-
-			//System.out.println("");
-			//p.showGrid();
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
 }
