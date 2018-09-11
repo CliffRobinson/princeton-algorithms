6323df4e1c824e835294bd8a975e72980d381181
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-09-11 19:09:52 +1200

    all working?

---
+++ src/Permutation.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+
+	public static void main(String[] args) {
+		
+		int k = 3;
+		
+		try {
+			k = Integer.parseInt(args[0]);
+		} catch (IndexOutOfBoundsException e){
+			StdOut.print("No args.");
+		}
+		
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		
+		while (!StdIn.isEmpty()) {
+			String butts = StdIn.readString();
+			//StdOut.print(butts+"\n");
+			rq.enqueue(butts);
+		}
+		
+
+		
+		for (int i = 0; i < k; i++) {
+			StdOut.println(rq.dequeue());
+		}
+		
+		
+
+	}
+
+}
